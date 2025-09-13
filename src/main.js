8190b9a02fc4bac4274d446a7c2be98bb2b0ce86
Abhinav Bijarnia <abhinav789.bij@gmail.com>
2025-09-13 15:06:27 +0530

    fixed webhistory and base path

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
@@ -26,7 +26,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory('/template_ocr_engine_ui/'),
     routes
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), tailwindcss(),],
-    base: 'https://abhinavkbij.github.io/template_ocr_engine_ui/',
+    // base: 'https://abhinavkbij.github.io/template_ocr_engine_ui/',
+    base: '/template_ocr_engine_ui/'
 })
